09e11fb9e2300ead83ca2f8e0e9f5aa8a903273f
Captain Jack Sparrow <15pwele4721@uetpeshawar.edu.pk>
2026-06-18 20:25:56 +0400

    first responder schema

---
+++ schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+
+class Reflection(BaseModel):
+    missing: str = Field(description="Critique of what is missing")
+    superfluous: str = Field(description="Critique of what is superfluous")
+
+
+
+class AnswerQuestion(BaseModel):
+    answer: str = Field(description="~250 words detailed answer to the question")
+    reflection: Reflection = Field(description="your reflection on the initial answer")
+    search_queries: list[str] = Field(description="1-3 search queries for researching improvements to address the critique of your current answer.")
+    
